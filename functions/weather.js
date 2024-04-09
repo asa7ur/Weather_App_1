fca50eddae9371361d5694ff71fd3af505e25cea
Garik Asatryan <asa7ur@gmail.com>
2024-04-09 17:53:16 +0200

    Added a serverless function to fetch info using OpenWeather API key

---
+++ functions/weather.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const axios = require('axios')
+
+const url = `https://api.openweathermap.org/data/2.5/forecast?appid=${process.env.API_KEY}&units=metric&q=`
+
+exports.handler = async (event, context) => {
+	const method = event.httpMethod
+	// console.log(method);
+
+	if (method !== 'POST') {
+		return {
+			statusCode: 405,
+			body: 'Only PUT Requests Allowed',
+		}
+	}
+	const { location } = JSON.parse(event.body)
+	try {
+		const resp = await axios.get(`${url}${location}`)
+		return {
+			statusCode: 200,
+			body: JSON.stringify(resp.data),
+		}
+	} catch (error) {
+		return {
+			statusCode: 404,
+			body: JSON.stringify(error),
+		}
+	}
+}
